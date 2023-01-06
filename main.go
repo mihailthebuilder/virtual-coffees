d4daf3138466ab3867d50acbf61d2db74860f6a1
Mihail Marian <mihailmarian12@gmail.com>
2023-01-06 16:26:24 +0000

    create voice channel for 2 users

--- main.go
+++ main.go
@@ -39,4 +39,33 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	getChannelDetails(botToken, serverId)
+}
+
+func getChannelDetails(botToken string, serverId string) {
+	url := fmt.Sprintf("https://discord.com/api/guilds/%s/channels/1035886233135620128", serverId)
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bot %s", botToken))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(os.Stdout, resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
