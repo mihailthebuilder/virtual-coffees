a42aa3d4b1d1590aadde8ab536fe03caf16b8e6e
Mihail Marian <mihailmarian12@gmail.com>
2023-01-06 16:41:55 +0000

    create multiple tables

--- main.go
+++ main.go
@@ -12,64 +12,38 @@ func main() {
 	botToken := os.Getenv("BOT_TOKEN")
 	serverId := os.Getenv("SERVER_ID")
 
-	getChannelDetails(botToken, serverId)
-	fmt.Println("")
-	createCoffeeTable(botToken, serverId)
+	createCoffeeTables(botToken, serverId, 5)
 }
 
-func createCoffeeTable(botToken string, serverId string) {
-	body := []byte(`{"name":"coffee-table","type":2,"user_limit":2,"parent_id":"1035886233135620127"}`)
+func createCoffeeTables(botToken string, serverId string, numberOfTables int) {
 
-	url := fmt.Sprintf("https://discord.com/api/guilds/%s/channels", serverId)
+	for i := 0; i < numberOfTables; i++ {
+		body := []byte(`{"name":"coffee-table","type":2,"user_limit":2,"parent_id":"1035886233135620127"}`)
 
-	client := &http.Client{}
+		url := fmt.Sprintf("https://discord.com/api/guilds/%s/channels", serverId)
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+		client := &http.Client{}
 
-	req.Header.Set("Authorization", fmt.Sprintf("Bot %s", botToken))
-	req.Header.Set("Content-Type", "application/json")
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer resp.Body.Close()
+		req.Header.Set("Authorization", fmt.Sprintf("Bot %s", botToken))
+		req.Header.Set("Content-Type", "application/json")
 
-	_, err = io.Copy(os.Stdout, resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
 
-func getChannelDetails(botToken string, serverId string) {
-	url := fmt.Sprintf("https://discord.com/api/guilds/%s/channels", serverId)
-	client := &http.Client{}
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	req.Header.Set("Authorization", fmt.Sprintf("Bot %s", botToken))
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer resp.Body.Close()
-
-	_, err = io.Copy(os.Stdout, resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	}
 }
